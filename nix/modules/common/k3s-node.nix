5c70af1370c70845f400d0076a7653445e665595
Chad Russell <chaddouglasrussell@gmail.com>
2025-05-23 17:59:14 -0400

    Refactor token file assignment in k3s-node.nix to remove unnecessary conditional expression for clarity. This change simplifies the token file configuration for agents and non-initial servers, enhancing maintainability.

--- nix/modules/common/k3s-node.nix
+++ nix/modules/common/k3s-node.nix
@@ -43,8 +43,7 @@ in
     serverAddr = lib.mkIf (role == "agent" || (role == "server" && !initialServer)) k3sServerUrl;
     # Token file is needed for agents and joining servers, using a single common token
     tokenFile = lib.mkIf (role == "agent" || (role == "server" && !initialServer))
-                  config.sops.secrets."k3s-node-token".path
-                else null; # No tokenFile for the initial server
+                  config.sops.secrets."k3s-node-token".path;
     extraFlags = [ "--flannel-iface=${tailscale.interface}" ];
   };
 
