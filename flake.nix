a0426404c8698de64b3247decf68b8d78426252a
Chad Russell <chaddouglasrussell@gmail.com>
2025-05-23 14:38:12 -0400

    Update flake.nix to change nixpkgs version from 25.05 to nixpkgs-unstable, allowing access to the latest features and improvements in the NixOS ecosystem.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS k3s cluster flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     disko.url = "github:nix-community/disko";
     sops-nix.url = "github:Mic92/sops-nix";
