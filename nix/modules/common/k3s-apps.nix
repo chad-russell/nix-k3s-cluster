5675d0ec7a14b6b11e0b0443ded32cdf8ba7c7e7
Chad Russell <chaddouglasrussell@gmail.com>
2025-05-23 14:26:40 -0400

    Refactor k3s-apps.nix to replace hello-world application manifests with a new test-minimal-app configuration. This update introduces a ConfigMap for simplified deployment, enhancing clarity and maintainability of the application setup.

--- nix/modules/common/k3s-apps.nix
+++ nix/modules/common/k3s-apps.nix
@@ -5,71 +5,17 @@
   # This configuration will only be applied on the k3s server node
   # because k3s-apps.nix is imported in k3s-server.nix profile.
   services.k3s.manifests = {
-    "hello-world-app" = {
-      # Replaced source with content
-      content = [
-        # Document 1: Deployment
-        {
-          apiVersion = "apps/v1";
-          kind = "Deployment";
-          metadata = {
-            name = "hello-world";
-            labels.app = "hello-world";
-          };
-          spec = {
-            replicas = 2;
-            selector.matchLabels.app = "hello-world";
-            template = {
-              metadata.labels.app = "hello-world";
-              spec.containers = [
-                {
-                  name = "nginx";
-                  image = "nginx:latest"; # Using a simple nginx image
-                  ports = [{ containerPort = 80; }];
-                }
-              ];
-            };
-          };
-        }
-        # Document 2: Service
+    "test-minimal-app" = {
+      content = [ # Using a list, as the original error was for a "doc"
         {
           apiVersion = "v1";
-          kind = "Service";
-          metadata.name = "hello-world-service";
-          spec = {
-            selector.app = "hello-world";
-            ports = [
-              {
-                protocol = "TCP";
-                port = 80;       # Internal port within the cluster for this service
-                targetPort = 80; # Port on the nginx pods
-              }
-            ];
-            type = "ClusterIP";
-          };
-        }
-        # Document 3: IngressRoute
-        {
-          apiVersion = "traefik.containo.us/v1alpha1";
-          kind = "IngressRoute";
+          kind = "ConfigMap";
           metadata = {
-            name = "hello-world-ingress";
-            namespace = "default"; # Assuming your app is in the default namespace
+            name = "test-configmap";
+            namespace = "default";
           };
-          spec = {
-            entryPoints = [ "web" ]; # Default Traefik entrypoint for HTTP
-            routes = [
-              {
-                match = "Host('hello-world.k3s.crussell.io')";
-                kind = "Rule";
-                services = [
-                  {
-                    name = "hello-world-service";
-                    port = 80;
-                  }
-                ];
-              }
-            ];
+          data = {
+            exampleKey = "exampleValue";
           };
         }
       ];
